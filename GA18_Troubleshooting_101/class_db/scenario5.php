2466de721620579b1a81e8df9f9bfaaa470fb0fd
derickbriones <dericknual.briones24@bicol-u.edu.ph>
2025-11-21 17:48:03 +0800

    Delete GA18_Troubleshooting_101/class_db/scenario5.php

--- GA18_Troubleshooting_101/class_db/scenario5.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","class_db");
-
-# The POST key was misspelled before, so we fix it to 'email'
-$email = $_POST['email'];
-
-$sql = "SELECT * FROM students WHERE email='$email'";
-$res = mysqli_query($conn, $sql);
-?>
