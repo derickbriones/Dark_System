761351a31af13ca4a2976abb20e1430178993ae6
derickbriones <dericknual.briones24@bicol-u.edu.ph>
2025-11-21 17:48:11 +0800

    Delete GA18_Troubleshooting_101/class_db/scenario6.php

--- GA18_Troubleshooting_101/class_db/scenario6.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","class_db");
-
-#We convert it into integer to prevent harmful input like ?id=0 OR 1=1.
-$id = intval($_GET['id']);
-$sql = "DELETE FROM students WHERE id = $id";
-mysqli_query($conn, $sql);
-?>
