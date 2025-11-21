5e185d454f596247b89602fc71500d95f8328330
derickbriones <dericknual.briones24@bicol-u.edu.ph>
2025-11-21 17:47:17 +0800

    Delete GA18_Troubleshooting_101/class_db/scenario13.php

--- GA18_Troubleshooting_101/class_db/scenario13.php
+++
@@ -1,7 +0,0 @@
-<?php
-$newEmail = $_POST['email'];
-
-#Without a WHERE clause, the update would affect ALL rows
-$sql = "UPDATE students SET email='$newEmail' WHERE student_id=$id";
-mysqli_query($conn,$sql);
-?>
