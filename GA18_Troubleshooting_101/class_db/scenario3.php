606b3b0b466683d05eb88a4fc71ee4fcb07a3cbb
derickbriones <dericknual.briones24@bicol-u.edu.ph>
2025-11-21 17:47:48 +0800

    Delete GA18_Troubleshooting_101/class_db/scenario3.php

--- GA18_Troubleshooting_101/class_db/scenario3.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","class_db");
-
-#We put prepared statement to protect the sql injection
-#If we put user input directly in sql is not safe
-$stmt = $conn->prepare("SELECT * FROM students WHERE age = ?");
-$stmt->bind_param("i", $age);
-$stmt->execute();
-?>
