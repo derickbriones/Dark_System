f77bce9e1c0b3c2a589d5347b435e6c948df384c
derickbriones <dericknual.briones24@bicol-u.edu.ph>
2025-11-21 17:46:58 +0800

    Delete GA18_Troubleshooting_101/class_db/scenario10.php

--- GA18_Troubleshooting_101/class_db/scenario10.php
+++
@@ -1,8 +0,0 @@
-<?php
-$age = $_POST['age'];
-
-#The original code misspelled the variable as 'aeg'
-#We fixed it to use the correct variable name 'age'
-$sql = "SELECT * FROM students WHERE age = $age";
-name
-?>
