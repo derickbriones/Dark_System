6f1b2e8b733aaa9c093b7793f986eb808db4e39e
derickbriones <dericknual.briones24@bicol-u.edu.ph>
2025-11-21 17:47:24 +0800

    Delete GA18_Troubleshooting_101/class_db/scenario14.php

--- GA18_Troubleshooting_101/class_db/scenario14.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data = $_POST;
-
-#Array keys must be written correctly inside the string
-#String values must also be placed inside quotes in SQL
-$sql = "INSERT INTO students (first_name, last_name, email)
-    VALUES ('{$data['first_name']}', '{$data['last_name']}', '{$data['email']}')";
-?>
